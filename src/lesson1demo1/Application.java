6a92e13e40dc33df4fb574d29b0bb11d64aef9e8
Farhad Islam <farhad.islam@outlook.com>
2017-11-15 23:21:00 +0000

    Lesson1demo1 comments added

--- src/lesson1demo1/Application.java
+++ src/lesson1demo1/Application.java
@@ -1,31 +1,37 @@
 package lesson1demo1;
 
-class Runner extends Thread{
-	
+/*Extend Thread class and write implementation of its run () method in the subclass
+Then invoke start () method   */
+
+class Runner extends Thread {
+
 	public void run() {
-		
-		for(int i = 0; i<10 ; i++) {
-			
+
+		for (int i = 0; i < 10; i++) {
+
 			System.out.println("Hello " + i);
-			
+
 			try {
 				Thread.sleep(10);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
 		}
 	}
 }
+
 public class Application {
 
 	public static void main(String[] args) {
-	System.out.println("First app");
-	
-	Runner r1 = new Runner();
-	r1.start();
-	Runner r2 = new Runner();
-	r2.start();
+		System.out.println("First app");
+
+		Runner r1 = new Runner();
+		r1.start();
+		Runner r2 = new Runner();
+		r2.start();
+		Runner r3 = new Runner();
+		r3.start();
 
 	}
 
