410a05ad8400c601daca1b947599515f2121a0f6
Farhad Islam <farhad.islam@outlook.com>
2017-11-15 23:29:36 +0000

    Lesson4 Added

---
+++ src/lesson4/Worker.java
@@ -0,0 +1,30 @@
+public class Worker {
+    private int count = 0;
+
+    public void main() {
+      System.out.println();
+
+    }
+
+    public void stageOne(){
+        try {
+            Thread.sleep(1);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+       new Thread(new Runnable(){
+           public void run(){
+                for (int i = 0; i < 100; i++) {
+                    count++;
+                }
+
+           }
+
+       });
+
+        
+     
+    }
+
+}
