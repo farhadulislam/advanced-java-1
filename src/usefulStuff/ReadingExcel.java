eef8840edbee37c976db245940012804da7874de
Farhad Ul Islam <farhad.islam@outlook.com>
2017-12-20 23:16:12 +0000

    ArrayList and HashMap interface created

--- src/usefulStuff/ReadingExcel.java
+++ src/usefulStuff/ReadingExcel.java
@@ -1,9 +1,19 @@
 package usefulStuff;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ReadingExcel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("....");
+		
+		List<Integer> arbitraryNumbers = new ArrayList<Integer>();
+		
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
 
 	}
 
