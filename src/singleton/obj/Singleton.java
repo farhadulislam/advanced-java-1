77dfb68b6f0e62e55b0aa21ce4f9b330f3c0405b
Farhad Ul Islam <farhad.islam@outlook.com>
2017-12-20 23:01:54 +0000

    Added a singleton class

---
+++ src/singleton/obj/Singleton.java
@@ -0,0 +1,17 @@
+package singleton.obj;
+
+public class Singleton {
+	
+    private static Singleton instance;
+    
+	public static Singleton getInstance () {
+		
+		if (instance == null) {
+			
+			Singleton singleton1 = getInstance();
+		}
+		return instance;
+		
+	}
+
+}
