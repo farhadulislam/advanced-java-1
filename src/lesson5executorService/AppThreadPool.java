fd386c2c4f985e575e28470e1364b6f2b9f6edc5
farhad islam <frhd@live.co.uk>
2018-01-15 15:50:59 +0000

    Lesson5 using Threadpool (executor services)

---
+++ src/lesson5executorService/AppThreadPool.java
@@ -0,0 +1,44 @@
+package lesson5executorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Processor implements Thread {
+    public void run() {
+        try {
+
+            Thread.sleep(100);
+
+        } catch(Exception e) {
+
+        }
+    }
+
+}
+
+public class AppThreadPool {
+
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    for(
+    int i = 0;
+    i<5;i++)
+
+    {
+
+        executor.submit(new Processor(i));
+
+        executor.shutdown();
+
+        System.out.println("All tasks submitted");
+        try {
+
+            executor.awaitTermination(1, TimeUnit.Days);
+
+        } catch(InterruptedException e) {
+
+        }
+    }
+
+
+}
