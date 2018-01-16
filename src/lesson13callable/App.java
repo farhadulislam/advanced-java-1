d2ea4c743a8817da5e52d024eccdc063cdba686e
farhad-islam <frhd@live.co.uk>
2018-01-16 16:04:29 +0000

    lesson13 Callable and Future

---
+++ src/lesson13callable/App.java
@@ -0,0 +1,49 @@
+package lesson13callable;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class App {
+
+    public static void main (String [] args) throws IOException {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Future<?> future = executor.submit((new Callable < Integer) > () {
+
+            public Integer call() throws Exception{
+
+                Random random = new Random();
+                int duration = random.nextInt(4000);
+
+                if(duration>2000) throw new IOException("Sleeping for too long");
+
+                System.out.println("Starting ...");
+
+                try{
+                    Thread.sleep(duration);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+                System.out.println("Finshed ...");
+
+                return duration;
+            }
+
+        });
+
+        executor.shutdown();
+
+        try{
+            System.out.println("Result is : " + future.get());
+
+        }catch (IOException e){
+
+        }finally {
+
+        }
+    }
+}
