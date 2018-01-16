560e5ff104a44ca6ba893777ade1866ca7fc6e10
farhad-islam <frhd@live.co.uk>
2018-01-16 10:42:52 +0000

    Lesson8 wait and notify

---
+++ src/Lesson8waitNotify/App.java
@@ -0,0 +1,12 @@
+package Lesson8waitNotify;
+
+public class App {
+
+    public static void main (String [] args){
+
+        new Processor.produce();
+        new Processor.consume();
+
+    }
+}
+
