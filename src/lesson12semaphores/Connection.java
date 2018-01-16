2cab4e48c7c03638272bd42ad482f4eebbf27126
farhad-islam <frhd@live.co.uk>
2018-01-16 15:22:01 +0000

    lesson12semaphores

---
+++ src/lesson12semaphores/Connection.java
@@ -0,0 +1,63 @@
+package lesson12semaphores;
+
+import java.util.concurrent.Semaphore;
+
+public class Connection {
+
+    private static Connection instance = new Connection();
+    private int connections = 0;
+    private Semaphore sem  = new Semaphore(10);
+
+
+    private Connection(){
+
+    }
+
+    public static Connection getInstance (){
+
+        return instance;
+    }
+    public void connect() throws InterruptedException {
+
+        sem.acquire();
+
+        try{
+            connect();
+
+        }finally {
+            sem.release();
+        }
+    }
+
+    public void doConnect() throws InterruptedException {
+
+
+
+        synchronized (this){
+
+            connections++;
+            System.out.println("Connections : " + connections);
+
+        }
+
+        try{
+
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+
+            e.printStackTrace();
+
+        }
+
+
+        synchronized (this){
+
+            connections--;
+            System.out.println("Connections : " + connections);
+        }
+
+
+
+
+    }
+}
