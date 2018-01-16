2c7fdd02067b5a40ff6856003bfcb1c41c789146
Farhad Ul Islam <farhad.islam@outlook.com>
2018-01-16 22:59:29 +0000

    Lesson8 quick fixes on obj reference

--- src/Lesson8waitNotify/App.java
+++ src/Lesson8waitNotify/App.java
@@ -4,8 +4,23 @@ public class App {
 
     public static void main (String [] args){
 
-        new Processor.produce();
-        new Processor.consume();
+    	
+    	Processor processor1 = new Processor ();
+    	Processor processor2 = new Processor ();
+       try {
+		processor1.produce();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+        try {
+			processor2.consume();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+     
 
     }
 }
