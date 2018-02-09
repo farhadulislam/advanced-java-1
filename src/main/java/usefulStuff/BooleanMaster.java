7eb1afdedbcc838c9905e4a6bbce580067a53c3f
farhad islam <frhd@live.co.uk>
2018-02-09 14:36:31 +0000

    Use of a static factory method and instantiating an abstract class

---
+++ src/main/java/usefulStuff/BooleanMaster.java
@@ -0,0 +1,21 @@
+package usefulStuff;
+
+public class BooleanMaster {
+    public static void main (String [] args){
+
+        boolean skyIsBlue = true ;
+        Boolean skyIsBlueObj = valueOf(a);
+
+        Animal animal = new Animal (){
+
+            String name = "humo sapiens";
+
+        };
+
+    }
+
+    public static Boolean valueOf(boolean a){
+
+        return a ? Boolean.TRUE: Boolean.FALSE;
+    }
+}
